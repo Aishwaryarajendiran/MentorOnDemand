91be043ca4b0782d9d11aa1c577411b196e7c8c9
HP <you@example.com>
2019-11-12 10:23:12 +0530

    Add project files.

---
+++ MOD.AuthLibrary/Migrations/20191111092350_InitialMigration1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MOD.AuthLibrary.Migrations
+{
+    public partial class InitialMigration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "19be2750-11b7-4bd0-a4e6-467ab9d07bf2");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "9ba2969e-4687-48a0-a4d7-8f634c849ad0");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "cc0ad250-2fe5-418d-90c0-4f8fb0a84bf3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1",
+                column: "ConcurrencyStamp",
+                value: "2ed3405a-7fce-4cac-ada9-2ac351284aed");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2",
+                column: "ConcurrencyStamp",
+                value: "449a81d8-ad00-4c5e-999d-064e391b62e3");
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "3",
+                column: "ConcurrencyStamp",
+                value: "ea282503-1464-4504-9712-a1d2c9390fd4");
+        }
+    }
+}
---
+++ MOD.AuthLibrary/Repositories/AdminRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MOD.AuthLibrary.Models;
+
+namespace MOD.AuthLibrary.Repositories
+{
+    public class AdminRepository : IAdminRepository
+    {
+        public AdminRepository()
+        {
+
+        }
+
+        bool IAdminRepository.AddSkill(Skill model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
